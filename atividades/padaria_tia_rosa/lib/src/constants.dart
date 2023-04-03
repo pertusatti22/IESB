da38289c234bfec5946c1229912bafe77c3a100a
pertusatti22 <pertusatti22@gmail.com>
2023-04-03 13:48:23 -0300

    add constants

---
+++ atividades/padaria_tia_rosa/lib/src/constants.dart
@@ -0,0 +1,4 @@
+const String title = 'Padaria tia rosa';
+const String logo = 'assets/images/logo.png';
+const String logoLite = 'assets/images/logo_lite.png';
+const String logoColor = 'assets/images/logo_color.png';
