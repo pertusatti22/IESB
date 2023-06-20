092b3e0d92aeb4c3b2a2efd6d60c61ed05137785
pertusatti22 <pertusatti22@gmail.com>
2023-06-20 20:09:38 -0300

    Add validação para inteiros na entrada

--- atividade_ativa/bin/atividade_ativa.dart
+++ atividade_ativa/bin/atividade_ativa.dart
@@ -1,5 +1,22 @@
-import 'package:atividade_ativa/atividade_ativa.dart' as atividade_ativa;
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${atividade_ativa.calculate()}!');
+  String? entrada;
+  int? lado;
+
+  for(int i = 1; i<=3;i++) {
+    while(true) {
+      print('Insira um número inteiro para o $i° lado do triângulo: ');
+      entrada = stdin.readLineSync();
+      if(entrada != null && entrada != '' && entrada != ' ') {
+        lado = int.tryParse(entrada);
+        if(lado != null) {
+          break;
+        }
+      } else {
+        print('Entrada nula');
+      }
+      print(lado);
+    }
+  }
 }
